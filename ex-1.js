2dc8ebda8a05a743c1bb085f4da5142f54094932
Supakit <book0970403815@hotmail.com>
2023-11-29 10:27:22 +0700

    feat : send assignment by book

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,23 @@
-// Start coding here
+function add(a, b) {
+  return a + b;
+}
+let number = add(10, 5);
+console.log(number);
+
+function subtract(a, b) {
+  return a - b;
+}
+let subtractNumber = subtract(10, 5);
+console.log(subtractNumber);
+
+function divide(a, b) {
+  return a / b;
+}
+let divideNumber = divide(10, 5);
+console.log(divideNumber);
+
+function multiple(a, b) {
+  return a * b;
+}
+let multipleNumber = multiple(10, 5);
+console.log(multipleNumber);
